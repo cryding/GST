876b8c06592e60a428a4d87b116b471a7221f33f
Babai malo <babaimalo122007@gmail.com>
2026-04-24 10:16:38 +0530

    Fix /mount/admin/install_path deployment issue by updating get_install_path function

--- Ecommerce.py
+++ Ecommerce.py
@@ -1,1 +1,5 @@
-# Your updated content for Ecommerce.py here
+def get_install_path():
+    import os
+    return os.path.join('/mount/admin', 'install_path')
+
+# Any other existing code you want to keep should be included here...
